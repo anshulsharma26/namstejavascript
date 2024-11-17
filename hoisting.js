ddde3f49c9c462f65de81cc582677be1395eb6b2
Anshul Sharma <ianshulsharma26@gmail.com>
2024-11-17 21:25:16 +0530

    hositing in js

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+// to understand hoisitng in javascript
+
+// hoisting is the phenomena in javascript that allow us to use variable even before they have been declared
+
+console.log(name)                                                             
+var name = 'Anshul Sharma'
+
+// output 
+// undefined
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Namste nodejs
+
+understand node in depth
