3661f70a59c0cd24c7c34f389675916438f9611f
Anshul Sharma <ianshulsharma26@gmail.com>
2024-11-17 21:53:42 +0530

    arrow function rest paramets and and spread operator

---
+++ es6.js
@@ -0,0 +1,32 @@
+// arrow function  and restparamete
+let myfun = (param1, param2, ...restParam)=>{
+    console.log(param1+param2)
+    console.log(restParam)
+}
+myfun(23,67,8,9,9)
+
+// spread operator with arr
+const arr = [ ...[81,78,34,67,78] , ... [7856,78] ]
+console.log(arr)
+
+// spread operator with object
+
+const personDetails = {
+    fatherName: "Anil Sharma",
+    motherName: "Savita Sharma",
+    dob: '26/05/2002'
+}
+const details = {
+    name: "Anshul",
+    gender: "mail"
+}
+const employeeDetails  = {...personDetails, ... details}
+console.log(employeeDetails)
+
+
+
+
+
+// array destructring
+
+
